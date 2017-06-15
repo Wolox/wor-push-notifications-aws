c859004ff629994a5e31d153798eaaf446548a72
Leandro Masello <leandro.masello@wolox.com.ar>
2017-06-15 15:22:38 -0300

    Add generator to add device tokens to users table (#7)
    * Add generator to add device tokens to users table
    
    Since push notifications work with registered devices, we are adding a json attribute to the users table which will contain this information.
    In further commits, we will modify this in order to allow customization of the table where device tokens will be saved.
    
    * Use versioned migrations when possible
    
    * Change homepage on gemspec file

---
+++ spec/wor/generators/install_generator_spec.rb
@@ -0,0 +1,35 @@
+require 'generator_spec'
+require 'generators/wor/push_notifications/aws/install_generator'
+
+describe Wor::PushNotifications::Aws::Generators::InstallGenerator, type: :generator do
+  context 'generating the initializer ' do
+    destination File.expand_path('../../../../tmp', __FILE__)
+
+    before(:all) do
+      prepare_destination
+      run_generator
+    end
+
+    let(:migration_version) do
+      "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]" if Rails.version.start_with? '5'
+    end
+    let(:current_migration_number) do
+      Wor::PushNotifications::Aws::Generators::InstallGenerator
+        .current_migration_number("#{destination_root}/db/migrate/")
+    end
+
+    it 'generates the correct structure for initializer' do
+      migration_file = "#{current_migration_number}_add_device_token_to_users.rb"
+      first_line = "class AddDeviceTokenJsonToUsers < ActiveRecord::Migration#{migration_version}"
+      expect(destination_root).to(have_structure{
+        directory 'db' do
+          directory 'migrate' do
+            file migration_file do
+              contains first_line
+            end
+          end
+        end
+      })
+    end
+  end
+end
