5da30423e4e8d974f97dc9c082eec25fc588ce4c
Leandro Masello <leandro.masello@wolox.com.ar>
2017-06-29 19:50:04 +0200

    Split PushNotification module into two (#11)
    * Split PushNotification module into two
    
    As ita requested, we should use the name wor-push-notifications-aws instead of wor-push_notifications-aws.
    This change requires to add an additional module and because of that, we have to change several conventions used in both modules and specs.
    
    It's good to know that, it was also proved with a dummy app and worked fine.
    
    * Add CircleCI config
    
    * Enable rubocop on spec dir

--- lib/wor/push-notifications/aws/version.rb
+++
@@ -1,7 +0,0 @@
-module Wor
-  module PushNotifications
-    module Aws
-      VERSION = '0.1.0'.freeze
-    end
-  end
-end
--- spec/wor/generators/install_generator_spec.rb
+++ spec/wor/generators/install_generator_spec.rb
@@ -1,9 +1,9 @@
 require 'generator_spec'
-require 'generators/wor/push-notifications/aws/install_generator'
+require 'generators/wor/push/notifications/aws/install_generator'
 
-describe Wor::PushNotifications::Aws::Generators::InstallGenerator, type: :generator do
+describe Wor::Push::Notifications::Aws::Generators::InstallGenerator, type: :generator do
   context 'generating the initializer ' do
-    destination File.expand_path('../../../../tmp', __FILE__)
+    destination File.expand_path('./../../../../tmp', __FILE__)
 
     before(:all) do
       prepare_destination
@@ -14,15 +14,15 @@ describe Wor::PushNotifications::Aws::Generators::InstallGenerator, type: :gener
       "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]" if Rails.version.start_with? '5'
     end
     let(:current_migration_number) do
-      Wor::PushNotifications::Aws::Generators::InstallGenerator
+      Wor::Push::Notifications::Aws::Generators::InstallGenerator
         .current_migration_number("#{destination_root}/db/migrate/")
     end
 
     it 'generates the correct structure for initializer' do
       migration_file = "#{current_migration_number}_add_device_token_to_users.rb"
       first_line = "class AddDeviceTokenJsonToUsers < ActiveRecord::Migration#{migration_version}"
-      third_line = "add_column :users, :device_tokens, :json, default: {}"
-      expect(destination_root).to(have_structure{
+      third_line = 'add_column :users, :device_tokens, :json, default: {}'
+      expect(destination_root).to(have_structure {
         directory 'db' do
           directory 'migrate' do
             file migration_file do
