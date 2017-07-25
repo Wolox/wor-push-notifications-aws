84d54845ff3035536e6c6198cebe060525ed0337
Leandro Masello <leandro.masello@wolox.com.ar>
2017-07-25 19:43:04 +0200

    Update rubocop and remove duplication of code (#24)

--- lib/wor/push/notifications/aws/ios_push_json_builder.rb
+++ lib/wor/push/notifications/aws/ios_push_json_builder.rb
@@ -6,13 +6,16 @@ module Wor
           class << self
             def build_json(message_content)
               unless Wor::Push::Notifications::Aws.aws_ios_sandbox
-                return { APNS:
-                  { aps: { alert: message_content[:message], badge: message_content[:badge],
-                           sound: 'default' } }.merge(message_content).to_json }
+                return { APNS: aps_content(message_content) }
               end
-              { APNS_SANDBOX:
-                { aps: { alert: message_content[:message], badge: message_content[:badge],
-                         sound: 'default' } }.merge(message_content).to_json }
+              { APNS_SANDBOX: aps_content(message_content) }
+            end
+
+            private
+
+            def aps_content(message_content)
+              { aps: { alert: message_content[:message], badge: message_content[:badge],
+                       sound: 'default' } }.merge(message_content).to_json
             end
           end
         end
